0fedf9ed12e42b420ff91280af95895d499e47e7
lyb <1185919199@qq.com>
2023-06-27 16:21:11 +0800

    List 链表部分

--- list.go
+++ list.go
@@ -354,7 +354,230 @@ func sortInList1(head *ListNode) *ListNode { //超时算法，选择排序O（n^
 	return thead.Next
 }
 
-// BM12 单链表的排序 分治法归并排序
+// BM12 单链表的排序 分治法归并排序利用数组进行分治法归并排序所使用的MergeSort在sort.go中实现
 func sortInList(head *ListNode) *ListNode {
+	n := 0
+	midhead := head
+	for head != nil {
+		head = head.Next
+		n += 1
+	}
+	if n == 1 {
+		return midhead
+	}
+	midval := make([]int, n)
+	i := 0
+	for midhead != nil {
+		midval[i] = midhead.Val
+		midhead = midhead.Next
+		i = i + 1
+	}
+	midval = MergeSort(midval)
+	thead := &ListNode{
+		Val:  midval[0],
+		Next: nil,
+	}
+	midhead = thead
+	for i := 1; i < len(midval); i++ {
+		tmp := &ListNode{
+			Val:  midval[i],
+			Next: nil,
+		}
+		thead.Next = tmp
+		thead = thead.Next
+	}
+	return midhead
+}
 
+// BM12 单链表排序，直接在链表上使用分治法归并排序
+func sortInList2(head *ListNode) *ListNode {
+	// write code here
+	if head == nil {
+		return head
+	}
+	return splitLink(head)
+}
+func splitLink(head *ListNode) *ListNode {
+	// 拆分为只剩下一个节点或者空节点
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var pre *ListNode
+	slow, fast := head, head
+	// 通过快慢指针把链表拆分为2部分
+	for fast != nil && fast.Next != nil {
+		pre = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	// pre为slow的前一个，将next指向为空断开
+	pre.Next = nil
+	// 使用递归方式分别拆分2部分链表
+	left := splitLink(head)
+	right := splitLink(slow)
+	return mergeLink(left, right)
+}
+func mergeLink(l1, l2 *ListNode) *ListNode {
+	// 合并两个有序的链表
+	// 创建新的空头节点
+	newH := &ListNode{}
+	node := newH
+	for l1 != nil && l2 != nil {
+		if l1.Val > l2.Val {
+			node.Next = l2
+			l2 = l2.Next
+		} else {
+			node.Next = l1
+			l1 = l1.Next
+		}
+		node = node.Next
+	}
+	// 直接拼接还没有遍历完，剩下的链表
+	if l1 != nil {
+		node.Next = l1
+	}
+	if l2 != nil {
+		node.Next = l2
+	}
+	return newH.Next
+}
+
+// BM13 判断链表是否为会问结构,借助数组，将链表值按顺序存入数组中，判断数组中值是否为回文结构
+func isPail(head *ListNode) bool {
+	// write code here
+	if head == nil {
+		return true
+	}
+	n := 0
+	midhead := head
+	for head != nil { //在此可以直接将节点值通过midaval = append(midval,head.val)进行赋值，但其可能消耗的内存空间较大
+		n += 1 //因为当切片当内村不足时，<1024时会以当前的两倍进行扩增，也会进行数据迁移复制，因此不如得到大小进行再赋值
+		head = head.Next
+	}
+	if n == 1 {
+		return true
+	}
+	midval := make([]int, n)
+	i := 0
+	for midhead != nil {
+		midval[i] = midhead.Val
+		i += 1
+		midhead = midhead.Next
+	}
+	for i := 0; i < n/2; i++ {
+		if midval[i] != midval[n-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+// BM14 链表的奇偶重排，意思是奇偶排在一起，先奇数后偶数
+func oddEvenList(head *ListNode) *ListNode { //利用两次循环
+	// write code here
+	if head == nil || head.Next == nil {
+		return head
+	}
+	thead := &ListNode{}
+	n := 1
+	mhead := thead
+	ohead := head
+	for head != nil {
+		if n%2 != 0 {
+			tmp := &ListNode{Val: head.Val}
+			thead.Next = tmp
+			thead = thead.Next
+		}
+		n += 1
+		head = head.Next
+	}
+	n = 1
+	for ohead != nil {
+		if n%2 == 0 {
+			tmp := &ListNode{Val: ohead.Val}
+			thead.Next = tmp
+			thead = thead.Next
+		}
+		n += 1
+		ohead = ohead.Next
+	}
+	return mhead.Next
+}
+func oddEvenList1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	oddHead := &ListNode{}
+	evenHead := &ListNode{}
+	odd := oddHead
+	even := evenHead
+	n := 1
+	for head != nil { //利用一次循环
+		tmp := &ListNode{Val: head.Val}
+		if n%2 != 0 {
+			oddHead.Next = tmp
+			oddHead = oddHead.Next
+		} else {
+			evenHead.Next = tmp
+			evenHead = evenHead.Next
+		}
+		n += 1
+		head = head.Next
+	}
+	oddHead.Next = even.Next
+	return odd.Next
+}
+
+// BM15 删除有序列表中的重复元素
+func deleteDuplicatesI(head *ListNode) *ListNode {
+	// write code here
+	if head == nil || head.Next == nil {
+		return head
+	}
+	thead := head
+	pre := head.Next
+	for head != nil && head.Next != nil {
+		for head.Val == pre.Val {
+			head.Next = pre.Next
+			pre = pre.Next
+			if pre == nil {
+				break
+			}
+		}
+		head = head.Next
+	}
+	return thead
+}
+
+// BM16 删除链表中所有重复出现的元素，只保留链表中只出现一次的元素
+func deleteDuplicates(head *ListNode) *ListNode {
+	// write code here
+	if head == nil || head.Next == nil {
+		return head
+	}
+	//preH 为当前值的前一个，以便移出去相同的值；mid为当前值的后一个，判断是否相等，直到找到不同的值
+	preH := &ListNode{}
+	preH.Next = head
+	thead := preH
+	mid := head.Next
+	for head != nil && head.Next != nil {
+		flag := 0
+		for head.Val == mid.Val {
+			flag = 1
+			mid = mid.Next
+			if mid == nil { //当到链表末尾时，意味着从当前到末尾全部一样，即舍去，将前一个指针指向空返回链表
+				preH.Next = nil
+				return thead.Next
+			}
+		}
+		if flag == 1 { //判断是否有相等的，若有，则直接将preH 移到该不同值的前一个，head指向不同的值位置
+			preH.Next = mid
+			head = mid
+		} else { //没有相等的则每一个向后移动一步
+			preH = preH.Next
+			head = head.Next
+		}
+		mid = head.Next
+	}
+	return thead.Next
 }
--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import "fmt"
 //牛客101
 func main() {
 	list := []int{1, 4, 3, 5, 2, 9, 0, 10, 7, 100}
-
-	// list1 := []int{3, 4, 5}
 	list = HeapSort(list)
 	fmt.Print(list)
+
 }
