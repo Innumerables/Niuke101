c437921fb0acac23f4bb14c16e96d32fa4cb38fe
lyb <1185919199@qq.com>
2023-07-26 11:52:48 +0800

    pointers

--- two_points.go
+++ two_points.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 //双指针算法，BM87-BM94
 
 //BM87 合并两个有序数组
@@ -44,4 +46,76 @@ func judge(str string) bool {
 		}
 	}
 	return true
+}
+
+//BM89 合并区间
+type Interval struct {
+	Start int
+	End   int
+}
+
+func merge0(intervals []*Interval) []*Interval {
+	// write code here
+	if len(intervals) < 2 {
+		return intervals
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start
+	})
+	var res []*Interval
+	res = append(res, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i].Start <= res[len(res)-1].End {
+			res[len(res)-1].End = max(intervals[i].End, res[len(res)-1].End)
+		} else {
+			res = append(res, intervals[i])
+		}
+	}
+	return res
+}
+
+//BM90最小覆盖字串
+func minWindow(S string, T string) string {
+	// write code here
+	s := S[:]
+	t := T[:]
+	tmap := map[byte]int{}
+	for i := 0; i < len(t); i++ {
+		tmap[t[i]]++
+
+	}
+	smap := map[byte]int{}
+	mid := []int{}
+	flag := 0
+	minlen := len(s)
+	start := 0
+	end := 0
+	tr := 0
+	for i := 0; i < len(s); i++ {
+		_, ok := tmap[s[i]]
+		if ok {
+			smap[s[i]]++
+			mid = append(mid, i) //用于存储与T值相对应所有S中的坐标，便与向前跳跃匹配
+			if smap[s[i]] == tmap[s[i]] {
+				flag++
+			}
+		}
+		for flag == len(tmap) {
+			tr = 1
+			if mid[len(mid)-1]-mid[0] < minlen { //获得最小的距离
+				minlen = mid[len(mid)-1] - mid[0]
+				start = mid[0]
+				end = mid[len(mid)-1]
+			}
+			smap[s[mid[0]]]--                      //减去满足条件时第一个位置所存的字符的次数
+			if smap[s[mid[0]]] < tmap[s[mid[0]]] { //若减去之后次数小于T中次数，则说明不满足条件跳出循环
+				flag--
+			}
+			mid = mid[1:] //然后删除第一个位置，若减去次数满足则继续循环，直到不满足，找到最小的
+		}
+	}
+	if tr == 0 {
+		return ""
+	}
+	return string(s[start : end+1])
 }
